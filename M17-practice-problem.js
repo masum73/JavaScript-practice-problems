1277d1a61f597c9f52307ed250b0bba21133cd26
Masum Rahman <masumrahman73@gmail.com>
2023-01-26 23:07:58 +0600

    first commit - day 1 - module 17 - practice problem all solved

---
+++ M17-practice-problem.js
@@ -0,0 +1,39 @@
+// Module 17-10, Practice Problem 1 - Addition , Subtraction
+
+var totalMoney = 1000; 
+var orangePrice = 300;
+var applePrice = 400; 
+
+console.log( totalMoney - (orangePrice + applePrice) );
+console.log('Problem 1 - Solved')
+
+// Module 17-10, Practice Problem 1 - Average 
+
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50; 
+
+var totalMarks = math + biology + chemistry + physics + bangla;
+
+var averageMarks = totalMarks / 5; 
+
+averageMarks = averageMarks.toFixed(2); // for 2 decimal points 
+
+console.log(averageMarks);
+console.log('Problem 2 - Solved')
+
+// Module 17-10, Practice Problem 3 - String concatenation  
+
+var firstLine = 'I am going to be';
+var secondLine = 'an awesome web developer';
+
+console.log(firstLine + ' ' + secondLine);
+console.log('Problem 3 - Solved')
+
+// Module 17-10, Practice Problem 4 - remainder
+
+var firstNumber = 119; 
+console.log(firstNumber % 5);
+console.log('Problem 4 - Solved')
