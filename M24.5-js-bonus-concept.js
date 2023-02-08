1c2ecdd7774f3b9811efd96a02f42e8e963e822c
Masum Rahman <masumrahman73@gmail.com>
2023-02-09 00:09:47 +0600

    M24.5 JS Bonus video practice done

--- M24.5-js-bonus-concept.js
+++ M24.5-js-bonus-concept.js
@@ -1,4 +1,4 @@
-// recursion using sum of numbers 
+// recursion using sum of numbers
 
 /* 
 incremental
@@ -42,7 +42,7 @@ console.log(result);
     15
 */
 
-// factorial 
+// factorial
 /*
 let factorial = 1 ;
 for(let i=5; i>=1; i--){
@@ -64,20 +64,70 @@ console.log(result2);
 
 //object
 const student = {
-    name: 'sakib',
-    id: 121,
-    address: 'movie',
-    isSingle: true,
-    friends: ['apu', 'salma', 'mou'],
-    car: {
-        brand: 'tesla',
-        price: 8000000,
-        model: 2022,
-        manufacturer: {
-            name: 'elon mask',
-            country: 'USA'
-        }
-    }
-}
+  name: "sakib",
+  id: 121,
+  address: "movie",
+  isSingle: true,
+  friends: ["apu", "salma", "mou"],
+  car: {
+    brand: "tesla",
+    price: 8000000,
+    model: 2022,
+    manufacturer: {
+      name: "elon mask",
+      country: "USA",
+    },
+  },
+};
 
 console.log(student);
+
+// arguments - array like object - ony function er vitore e pawa jai
+
+function add(num1, num2) {
+  console.log(num1, num2);
+  console.log(arguments);
+  console.log(arguments[3]);
+}
+
+add(12, 13, 45, 67, 676, 54);
+
+// array - object but not array
+const products = {
+  0: 15,
+  1: 56,
+  2: 89,
+};
+
+const numbers = [23, 44, 56, 66, 32, 224, 34, 555];
+
+for (let i = 0; i < numbers.length; i++) {
+  const number = numbers[i];
+  console.log(number);
+}
+// for of loop
+for (const number of numbers) {
+  console.log(number);
+}
+
+const myProducts = [
+    {id: 1, name:'mi', price: 23000},
+    {id: 2, name:'Nokia phone', price: 32000},
+    {id: 3, name:'Samsung', price: 34000},
+    {id: 4, name:'Oppo PHone', price: 55000},
+]
+
+
+
+function matchedProducts(myProducts, search){
+    const matched =[];
+    for(const myProduct of myProducts){
+       if(myProduct.name.toLowerCase().includes(search.toLowerCase())){
+            matched.push(myProduct);
+       }
+    }
+    return matched;
+}
+
+const result3 = matchedProducts(myProducts, 'phone')
+console.log(result3);
